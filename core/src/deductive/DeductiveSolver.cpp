2676214b79f020ad0df11d2e305dbe8ddb5c4acd
mzexedell <mzexe.dell@gmail.com>
2025-12-29 21:54:56 +0300

    minor comment edit

--- core/src/deductive/DeductiveSolver.cpp
+++ core/src/deductive/DeductiveSolver.cpp
@@ -10,7 +10,7 @@ namespace epuzzle::details::deductive
 
     std::vector<PuzzleSolution> DeductiveSolver::solve(SolveOptions)
     {
-        // TODO: Implement it! Something like Backtracking + Branch Pruning. Instead of bruteforce::ConstraintChecker it will use ConstraintPropagator
+        // TODO: Implement it! 
         // 
         // And don't forget to remove GTEST_SKIP() from SolverTests::SetUp() and SolverRealPuzzleTests::SetUp()!
 
